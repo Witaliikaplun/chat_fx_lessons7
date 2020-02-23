d54f1903498a8c6746e7cf253ef902ebca7ef5b3
Witaliikaplun <vitalijkaplun@gmail.com>
2020-02-23 19:58:13 +0300

    Initial commit

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,117 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    Socket socket = null;
+    DataInputStream in;
+    DataOutputStream out;
+    Server server;
+    private String nick;
+    private String login;
+
+    public ClientHandler(Socket socket, Server server) {
+        try {
+            this.socket = socket;
+            this.server = server;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    //цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/auth ")) {
+                            String[] token = str.split(" ");
+//                            String[] token1 = str.split(" ",3);
+                            String newNick = server
+                                    .getAuthService()
+                                    .getNicknameByLoginAndPassword(token[1], token[2]);
+                            if (newNick != null) {
+                                sendMsg("/authok " + newNick);
+                                nick = newNick;
+                                login = token[1];
+                                server.subscribe(this);
+                                System.out.println("Клиент " + nick + " подключился");
+                                break;
+                            } else {
+                                sendMsg("Неверный логин / пароль");
+                            }
+                        }
+                    }
+
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            out.writeUTF("/end");
+                            break;
+                        }
+                        //индивидуальные сообщения--------------------------------------
+                        if (str.startsWith("/w")) {
+                            String[] str1 = str.split(" ", 3);
+
+                            String str2 = str1[1].substring(4);
+                            int number = 0;
+                            try {
+                                number = Integer.parseInt(str2);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Не верный формат сообщения!");
+                                out.writeUTF("Не верный формат сообщения!");
+                            }
+
+                            System.out.println("numer: " + number);
+                            try {
+                                server.sendPrivateMsg(str.substring(9), number-1);
+                            } catch (ArrayIndexOutOfBoundsException ex){
+                                out.writeUTF("Не допустимый номер пользователя!");
+                            }
+
+                        } else
+                            server.broadcastMsg(str);
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    server.unsubscribe(this);
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Клиент отключился");
+                }
+            }).start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
